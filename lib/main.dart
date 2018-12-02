0e32bbdc248a02cc8bc0f5c0e140c3f483e63eee
m-zaink <sidsmith9@gmail.com>
2018-12-02 18:02:59 +0530

    Adding padding in main.dart and added some colors to the FloatingActionButton.

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,24 @@ class HomePage extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text("Hello World", style: TextStyle(fontFamily: "Gamja Flower", fontSize: 30),),
+        title: Text(
+          "Hello World",
+          style: TextStyle(fontFamily: "Gamja Flower", fontSize: 30),
+        ),
       ),
-      body: Text(
-        "Hello World. I'm the new app from inside flutter.",
-        style: TextStyle(fontSize: 30, fontFamily: "Gamja Flower"),
-      ),
+      body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Text(
+            "Hello World. I'm the new app from inside flutter.",
+            style: TextStyle(fontSize: 30, fontFamily: "Gamja Flower"),
+          )),
       floatingActionButton: FloatingActionButton(
-        child: Text("Press"),
-        onPressed: () {
-          print("Button pressed.");
-        },
+        onPressed: () => print("Floating Button Pressed."),
+        child: Text(
+          "Press",
+          style: TextStyle(fontFamily: "Gamja Flower", fontSize: 20),
+        ),
+        backgroundColor: Colors.red,
       ),
     ));
   }
