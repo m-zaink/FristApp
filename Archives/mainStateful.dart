a04d1f239ba5ebd596dfda1a98eeb0fbfb4fa1f8
m-zaink <sidsmith9@gmail.com>
2018-12-03 16:00:13 +0530

    Reduced the amount of code in main.dart
    Amount of code in main.dart was reduced comparatively to the last commit.

---
+++ Archives/mainStateful.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Hello World",
+          style: TextStyle(fontFamily: "Gamja Flower", fontSize: 30),
+        ),
+      ),
+      body: Column(children: [
+        Column(
+          children: _products
+              .map((element) => Card(
+                      child: Column(
+                    children: [
+                      Image.asset("assets/eiffel.jpg"),
+                      Text(
+                        element,
+                        style:
+                            TextStyle(fontSize: 30, fontFamily: "Gamja Flower"),
+                      ),
+                    ],
+                  )))
+              .toList(),
+        )
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _products.add('Advanced Food Tester');
+          });
+
+          print(_products);
+        },
+        child: Text(
+          "Press",
+          style: TextStyle(fontFamily: "Gamja Flower", fontSize: 20),
+        ),
+        backgroundColor: Colors.red,
+      ),
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,47 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _MyAppState();
   }
 }
 
 class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
-
+  List<String> _products = ['Hello world'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Hello World",
-          style: TextStyle(fontFamily: "Gamja Flower", fontSize: 30),
-        ),
-      ),
-      body: Column(children: [
-        Column(
-          children: _products
-              .map((element) => Card(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Stateful App")),
+        body: Column(
+            children: _products
+                .map((element) => Card(
                       child: Column(
-                    children: [
-                      Image.asset("assets/eiffel.jpg"),
-                      Text(
-                        element,
-                        style:
-                            TextStyle(fontSize: 30, fontFamily: "Gamja Flower"),
+                        children: <Widget>[
+                          Image.asset("assets/eiffel.jpg"),
+                          Text(
+                            element,
+                            style: TextStyle(
+                                fontFamily: "Gamja Flower",
+                                fontSize: 30,
+                                color: Colors.black),
+                          )
+                        ],
                       ),
-                    ],
-                  )))
-              .toList(),
-        )
-      ]),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _products.add('Advanced Food Tester');
-          });
-
-          print(_products);
-        },
-        child: Text(
-          "Press",
-          style: TextStyle(fontFamily: "Gamja Flower", fontSize: 20),
+                    ))
+                .toList()),
+        floatingActionButton: FloatingActionButton(
+          child: Text("Press"),
+          onPressed: () {
+            setState(() {
+              _products.add("Some more random stuff.");
+            });
+          },
         ),
-        backgroundColor: Colors.red,
       ),
-    ));
+    );
   }
 }
