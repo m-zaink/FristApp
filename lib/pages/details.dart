16eb452fef3696aa95907490e74fb46354c49a35
m-zaink <sidsmith9@gmail.com>
2018-12-23 17:16:10 +0530

    Just another comment related commit

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Details extends StatelessWidget {
+  //details shall receive the set of details to be displayed for a 
+  //product so selected.
   final String details;
+  //Just a global variable for text styles.
   final TextStyle myStyle = TextStyle(fontFamily: 'PatuaOne');
+  //Function reference to delete a product.
   final Function deleteProduct;
+  //This stores the index of the card to be deleted.
   final int index;
 
+  //Constructor
   Details(this.details, this.deleteProduct, this.index);
 
+
+  //Regular build method.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
